9fac4e948ee37e09042ca0fa426b1c746167cadb
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-04-22 17:01:15 +0200

    opening drawer

--- client-pwa/src/sections/Navbar.tsx
+++ client-pwa/src/sections/Navbar.tsx
@@ -1,8 +1,9 @@
 import LongLogo from '../components/LongLogo.tsx';
 import styles from '../styles/navbar.module.css'
 import MenuButton from "../components/MenuButton.tsx";
-import {Button} from "@mui/material";
+import {Button, Drawer} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import {useState} from "react";
 
 
 /**
@@ -11,23 +12,31 @@ import MenuIcon from '@mui/icons-material/Menu';
  */
 const Navbar = () => {
 
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
     return (
-        <div className={styles.navbarContainer}>
-            <LongLogo/>
-            <div className={styles.menuButtons}>
-                <MenuButton text={"Home"} />
-                <MenuButton text={"Boardgames"} />
-                <MenuButton text={"Play"} />
-                <MenuButton text={"Profile"} />
-                <MenuButton text={"About"} />
+        <nav>
+            <div className={styles.navbarContainer}>
+                <LongLogo/>
+                <div className={styles.menuButtons}>
+                    <MenuButton text={"Home"} />
+                    <MenuButton text={"Boardgames"} />
+                    <MenuButton text={"Play"} />
+                    <MenuButton text={"Profile"} />
+                    <MenuButton text={"About"} />
+                </div>
+                <div className={styles.menuToggle}>
+                    <Button onClick={() => setDrawerOpen(!drawerOpen)}>
+                        <MenuIcon />
+                    </Button>
+                </div>
             </div>
-            <div className={styles.menuToggle}>
-                <Button>
-                    <MenuIcon />
-                </Button>
-            </div>
-        </div>
+            <Drawer open={drawerOpen} onClose={() => setDrawerOpen(false)}>
+                <div className={styles.drawerContainer}>
+
+                </div>
+            </Drawer>
+        </nav>
     )
 }
 
