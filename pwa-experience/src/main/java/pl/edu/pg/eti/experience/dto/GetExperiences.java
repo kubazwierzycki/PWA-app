3815f8edba7fd4668e5de1d746e11d586f40200e
kubazwierzycki <133905844+kubazwierzycki@users.noreply.github.com>
2024-05-16 21:33:59 +0200

    create dto

---
+++ pwa-experience/src/main/java/pl/edu/pg/eti/experience/dto/GetExperiences.java
@@ -0,0 +1,38 @@
+package pl.edu.pg.eti.experience.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Used as a GET response to represent one or more statistic sets
+ */
+@Getter
+@Setter
+@AllArgsConstructor(access= AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Builder
+@ToString
+public class GetExperiences {
+    @Getter
+    @Setter
+    @AllArgsConstructor(access= AccessLevel.PRIVATE)
+    @NoArgsConstructor
+    @Builder
+    @ToString
+    public static class Experience {
+        private UUID uuid;
+        private String username;
+        private String game;
+        private int numberOfPlays;
+    }
+
+    private List<Experience> experiences;
+}
