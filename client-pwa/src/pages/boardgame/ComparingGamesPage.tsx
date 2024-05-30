e3a9ad5d1b4259ca1f04c1c87cd2d347fd0ccc9f
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-05-30 23:01:46 +0200

    page layout

--- client-pwa/src/pages/boardgame/ComparingGamesPage.tsx
+++ client-pwa/src/pages/boardgame/ComparingGamesPage.tsx
@@ -1,11 +1,35 @@
+import styles from "../../styles/comparing.module.css"
+import {Button, Typography} from "@mui/material";
 
 
 const ComparingGamesPage = () => {
 
 
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <Typography variant="h4">
+                    Choose better game:
+                </Typography>
+            </div>
+            <div className={styles.wrapper}>
+                <div className={styles.cardContainer}>
 
+                </div>
+                <div className={styles.vs}>
+                    <Typography variant="h5">
+                        vs.
+                    </Typography>
+                </div>
+                <div className={styles.cardContainer}>
+
+                </div>
+            </div>
+            <div className={styles.next}>
+                <Button variant="outlined">
+                    Next pair
+                </Button>
+            </div>
         </div>
     )
 }
