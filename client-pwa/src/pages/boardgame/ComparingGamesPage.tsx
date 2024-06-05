9e72c617cfdc619e1a7f5863caf593ee9c5da1a0
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-06-05 22:20:41 +0200

    modal finish and save popover

--- client-pwa/src/pages/boardgame/ComparingGamesPage.tsx
+++ client-pwa/src/pages/boardgame/ComparingGamesPage.tsx
@@ -1,8 +1,9 @@
 import styles from "../../styles/comparing.module.css"
-import {Button, Typography} from "@mui/material";
+import {Button, Modal, Stack, Typography} from "@mui/material";
 import BoardGameCard from "../../components/BoardGameCard.tsx";
 import {useState} from "react";
-import image from "../../assets/test_thumbnail.webp" // testing only
+import image from "../../assets/test_thumbnail.webp"
+import {Box} from "@mui/system"; // testing only
 
 
 const ComparingGamesPage = () => {
@@ -10,9 +11,27 @@ const ComparingGamesPage = () => {
     // state of current pair choice
     const [chosen, setChosen] = useState(0);
 
+    // finish modal popup
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleModalClose = () => setModalOpen(false);
+
+    const handleModalOpen = () => setModalOpen(true);
+
     const handleCardClick = (val: number) => {
         setChosen(val);
     }
+
+    const style = {
+        position: 'absolute' as 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        boxShadow: 24,
+        p: 4,
+    };
 
     return (
         <div className={styles.container}>
@@ -47,13 +66,53 @@ const ComparingGamesPage = () => {
                 </div>
             </div>
             <div className={styles.next}>
-                <Button variant="contained" sx={{marginRight: "20px"}}>
+                <Button variant="contained" sx={{marginRight: "20px"}} onClick={handleModalOpen}>
                     Finish and Save
                 </Button>
                 <Button variant="outlined">
                     Next pair
                 </Button>
             </div>
+            <Modal
+                open={modalOpen}
+                onClose={handleModalClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <Typography id="modal-modal-title" variant="h6" component="h2" textAlign="center">
+                        Finish and save
+                    </Typography>
+                    <Typography id="modal-modal-description" sx={{ mt: 2, marginBottom: "10px" }}>
+                        Are you sure you want finish comparing and save your ranking update?
+                    </Typography>
+                    <div className={styles.buttonsContainer}>
+                        <Stack direction="row" spacing={1}>
+                            <Button
+                                variant="contained"
+                                color="warning"
+                                onClick={() => setModalOpen(false)}
+                            >
+                                Cancel
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="error"
+                                onClick={() => setModalOpen(false)} // to be changed
+                            >
+                                Discard
+                            </Button>
+                            <Button
+                                variant="contained"
+                                color="success"
+                                onClick={() => setModalOpen(false)} // to be changed
+                            >
+                                Save
+                            </Button>
+                        </Stack>
+                    </div>
+                </Box>
+            </Modal>
         </div>
     )
 }
