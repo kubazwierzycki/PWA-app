0918f86c95c0b49669511bb7c25c90a805b92c3c
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-08-22 18:19:15 +0200

    main banner text content

--- client-pwa/src/components/home/CapabilitiesSlider.tsx
+++ client-pwa/src/components/home/CapabilitiesSlider.tsx
@@ -1,5 +1,5 @@
 import styles from "../../styles/home.module.css"
-import {Tab, Tabs} from "@mui/material";
+import {Tab, Tabs, Typography} from "@mui/material";
 import {SyntheticEvent, useState} from "react";
 
 
@@ -11,6 +11,25 @@ const CapabilitiesSlider = () => {
         setValue(newValue);
     };
 
+    const tabsContent = [
+        {
+            header: "Seamlessly Sync Your Collection",
+            text: "Link your BoardGameGeek account and effortlessly manage your board game collection. Stay updated with the latest game data, and track your ever-growing library of tabletop treasures."
+        },
+        {
+            header: "Rate, Rank, and Discover",
+            text: "Use our intuitive ranking system to compare and rate your games. Our unique algorithm lets you rank your collection through quick, easy comparisons, helping you identify your favorites and discover hidden gems."
+        },
+        {
+            header: "Enhance Your Game Nights",
+            text: "Transform your game night with our Playroom tools. From customizable timers to streamlined scorekeeping, our app makes it easier than ever to focus on the fun while staying organized."
+        },
+        {
+            header: "Wizard-Driven Game Selection",
+            text: "Not sure what to play? Let our intelligent wizard choose the perfect game for your group based on your collection, player preferences, and the mood of the evening. Say goodbye to decision fatigue and hello to the best game experience."
+        }
+    ]
+
     const CustomTabPanel = ({index}: {index: number}) => {
         return (
             <div
@@ -21,8 +40,23 @@ const CapabilitiesSlider = () => {
             >
                 {
                     value === index &&
-                    <div>
-                        {index}
+                    <div className={styles.tabContainer}>
+                        <div className={styles.tabImg}>
+
+                        </div>
+                        <div className={styles.tabText}>
+                            <div>
+                                <Typography variant="h5" align="center">
+                                    {tabsContent[index].header}
+                                </Typography>
+                            </div>
+                            <hr/>
+                            <div>
+                                <Typography variant="body1" align="justify">
+                                    {tabsContent[index].text}
+                                </Typography>
+                            </div>
+                        </div>
                     </div>
                 }
             </div>
@@ -38,7 +72,7 @@ const CapabilitiesSlider = () => {
                     variant="scrollable"
                     scrollButtons="auto"
                 >
-                    <Tab label="Connect with BGG" />
+                    <Tab label="Connect with BGG"/>
                     <Tab label="Rank Games" />
                     <Tab label="Playroom" />
                     <Tab label="Best game for You" />
