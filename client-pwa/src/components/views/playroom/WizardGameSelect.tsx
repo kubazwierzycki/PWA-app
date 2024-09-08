4fbf4479df5754efc63774d46d64bc6f9258f032
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-09-08 21:08:10 +0200

    create playroom wizard ui

--- client-pwa/src/components/views/playroom/WizardGameSelect.tsx
+++ client-pwa/src/components/views/playroom/WizardGameSelect.tsx
@@ -1,17 +1,48 @@
-import {Typography} from "@mui/material";
+import {Stack, TextField, Typography} from "@mui/material";
+import {useState} from "react";
 
 
 const WizardGameSelect = () => {
 
+    const [minPlayTime, setMinPlayTime] = useState("0");
+    const [maxPlayTime, setMaxPlayTime] = useState("60");
 
     return (
-        <div>
-            <Typography width="100%" textAlign="center" lineHeight="100px">
-                Wizard
-            </Typography>
-            {
-                // TODO: interface for game selection wizard
-            }
+        <div style={{width: "100%", height: "100%", display: "flex", flexDirection: "column", padding: "20px 40px"}}>
+            <div style={{flex: 1}}>
+                <Stack direction="row" alignItems={"center"} spacing={2}>
+                    <Typography>
+                        Number of players:
+                    </Typography>
+                    <TextField value={"4"} disabled/>
+                </Stack>
+            </div>
+            <div style={{flex: 1}}>
+                <Stack direction="row" alignItems={"center"} spacing={2}>
+                    <Typography>
+                        Players age:
+                    </Typography>
+                    <TextField value={"19, 22, 23, 14"} disabled/>
+                </Stack>
+            </div>
+            <div style={{flex: 1}}>
+                <Stack direction="row" alignItems={"center"} spacing={2}>
+                    <Typography>
+                        Min play time:
+                    </Typography>
+                    <TextField
+                        value={minPlayTime}
+                        onChange={event => setMinPlayTime(event.target.value)}
+                    />
+                    <Typography>
+                        Max play time:
+                    </Typography>
+                    <TextField
+                        value={maxPlayTime}
+                        onChange={event => setMaxPlayTime(event.target.value)}
+                    />
+                </Stack>
+            </div>
         </div>
     )
 }
