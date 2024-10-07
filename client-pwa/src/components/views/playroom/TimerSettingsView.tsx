a2a91fef2359274f1742ddb472391c4a3028e8cc
ColdYellowTea <75476218+ColdYellowTea@users.noreply.github.com>
2024-10-07 13:45:06 +0200

    add timer setup

--- client-pwa/src/components/views/playroom/TimerSettingsView.tsx
+++ client-pwa/src/components/views/playroom/TimerSettingsView.tsx
@@ -1,6 +1,5 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { Button, Checkbox, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, FormControl, FormControlLabel, InputLabel, OutlinedInput} from "@mui/material";
 import { ReactNode} from "react";
-
 
 const TimerSettingsView = ({isGlobalTimer, timer, isTimerDialogOpen,
         setIsGlobalTimer, setTimer, setIsTimerDialogOpen, setIsTimerSet}: 
@@ -23,6 +22,14 @@ const TimerSettingsView = ({isGlobalTimer, timer, isTimerDialogOpen,
         handleClose();
     };
 
+    const handleGlobalTimerChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+      setIsGlobalTimer(event.target.checked)
+    }
+
+    const handleTimeChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+      setTimer(event.target.valueAsNumber)
+    }
+
 
 
     return(
@@ -40,7 +47,29 @@ const TimerSettingsView = ({isGlobalTimer, timer, isTimerDialogOpen,
             <DialogContentText id="alert-dialog-description">
               Please set up timer for your playroom.
             </DialogContentText>
-            {timer} {isGlobalTimer}
+            <FormControlLabel control={
+                    <Checkbox
+                    checked={isGlobalTimer}
+                    onChange={handleGlobalTimerChange}
+                    inputProps={{ 'aria-label': 'controlled' }}
+                  />
+              } label="Global timer" />
+              <FormControl
+                sx={{ m: 1 }}
+                variant="filled"
+                color="primary"
+                required={true}
+              >
+              <InputLabel shrink htmlFor="timerSetup">
+                  Time (seconds)
+              </InputLabel>
+              <OutlinedInput
+                  id="timerSetup"
+                  type={"number"}
+                  value={timer}
+                  onChange={handleTimeChange}
+              />
+            </FormControl>
           </DialogContent>
           <DialogActions>
             <Button onClick={handleClose}>Go back</Button>
