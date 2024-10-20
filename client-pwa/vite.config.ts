8233a4d2085490a3f80f48a76ec00069cc31f67c
Jakub Bednarz <kubabednarz1709@gmail.com>
2024-10-20 20:45:50 +0200

    basic manifest

--- client-pwa/vite.config.ts
+++ client-pwa/vite.config.ts
@@ -12,8 +12,27 @@ export default defineConfig({
     plugins: [
         react(),
         VitePWA({
+            registerType: 'autoUpdate',
             manifest: {
-                // manifest settings
+                name: "CoGame - Your personal boardgames center",
+                short_name: "CoGame",
+                description: "Unlock the full potential of your board game collection with our all-in-one companion app! Seamlessly connect with your BoardGameGeek account to manage your collection, rank games with a quick comparison tool, and enhance your game nights with custom timers and scorekeeping. Use our wizard feature to find the best games for any occasion, ensuring every play session is a win. Perfect for board game enthusiasts looking to take their hobby to the next level!",
+                start_url: "/",
+                display: "minimal-ui",
+                background_color: "#ffffff",
+                theme_color: "#304064",
+                icons: [
+                    {
+                        src: "/icons/icon-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
             },
             workbox: {
                 // workbox settings
