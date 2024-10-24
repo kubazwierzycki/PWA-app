c9a8607e07c5a1afdbc3a526704b154c1e73ca11
ColdYellowTea <75476218+ColdYellowTea@users.noreply.github.com>
2024-10-25 00:29:11 +0200

    add new search view

---
+++ client-pwa/src/components/views/playroom/BggSearchGameSelect.tsx
@@ -0,0 +1,130 @@
+import {alpha, Box, InputBase, List, ListItem, styled, Typography} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+import React, {Dispatch, ReactNode, SetStateAction, useEffect, useState} from "react";
+import bggService, { BggGameFromXML } from "../../../services/bgg"
+import axios from "axios";
+
+const Search = styled('div')(({theme}) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({theme}) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '100%',
+        },
+    },
+}));
+
+interface SearchSelectProps {
+    setName: Dispatch<SetStateAction<string>>
+    choice: string
+    setChoice: Dispatch<SetStateAction<string>>
+}
+
+/**
+ * View presenting searchbar functionality as means of choosing game to play in a playroom
+ * (Alternative to wizard choice)
+ * @param {Dispatch<SetStateAction<string>>} setName - callback for game choice info
+ * @param {string} choice - id of chosen game
+ * @param {Dispatch<SetStateAction<string>>} setChoice - id of chosen game change callback
+ * @returns {ReactNode}
+ */
+const BggSearchGameSelect = ({setName, choice, setChoice}: SearchSelectProps): ReactNode => {
+
+    const [input, setInput] = useState<string>("");
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(event.currentTarget.value);
+    }
+
+    const [bggGamesFromXML, setBggGamesFromXML] = useState<BggGameFromXML[] | null>(null);
+
+
+    useEffect(()=>{
+        // elminate race condition
+        const controller = new AbortController();
+
+        const fetchBggGamesByPattern = async() =>{
+            try{
+                const XML = await bggService.getBggGamesXMLByPatten(input, controller.signal);
+       
+                setBggGamesFromXML( await bggService.getAllGamesFromXML(XML));
+                
+            } catch(err) {
+                if (axios.isAxiosError(err)) {
+                    switch (err.code) {
+                        default:
+                            console.log(err.code)
+                            break;
+                    }
+                }
+            }
+        }
+
+        if(input.length >= 3){
+            
+            fetchBggGamesByPattern();
+        }
+
+        return () => controller.abort();
+    }, [input])
+
+    return (
+        <Box style={{height: "100%"}}>
+            <Search>
+                <SearchIconWrapper>
+                    <SearchIcon />
+                </SearchIconWrapper>
+                <StyledInputBase
+                    placeholder="Search…"
+                    inputProps={{ 'aria-label': 'search' }}
+                    value={input}
+                    onChange={handleInputChange}
+                />
+            </Search>
+            {
+            bggGamesFromXML ? 
+            <List sx={{
+                width: '100%',
+                bgcolor: 'background.paper',
+                position: 'relative',
+                overflow: 'auto',
+                maxHeight: 250,
+            }}>
+             {bggGamesFromXML.map(game => <ListItem>{game.name}</ListItem>)}
+            </List> : null
+            }
+        </Box>
+    )
+}
+
+export default BggSearchGameSelect;
