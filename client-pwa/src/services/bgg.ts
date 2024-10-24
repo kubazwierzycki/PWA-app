df2c37051098187b3b7f97cb1991d1ac842a9b6f
ColdYellowTea <75476218+ColdYellowTea@users.noreply.github.com>
2024-10-25 00:28:44 +0200

    add search to bgg service

--- client-pwa/src/services/bgg.ts
+++ client-pwa/src/services/bgg.ts
@@ -2,6 +2,48 @@ import axios from "axios";
 import api_address from "../config/api_address";
 const baseUrl = api_address.bgg;
 
+export interface BggGameFromXML{
+    name: string
+    gameId: string
+}
+
+const getAllGamesFromXML = async (res: string): Promise<BggGameFromXML[]> =>  {
+    console.log(res);
+    const xmlDoc = new DOMParser().parseFromString(res, "text/xml");
+    const xpathExpr = "/items/item/name/@value";
+    const xpathResult = document.evaluate(
+        xpathExpr,
+        xmlDoc,
+        null,
+        XPathResult.UNORDERED_NODE_ITERATOR_TYPE,
+        null
+    );
+    console.log(xpathResult);
+
+    let node;
+    const result : BggGameFromXML[]   = [];
+    // eslint-disable-next-line no-cond-assign
+    while (node = xpathResult.iterateNext()) {
+        result.push({"name":node.textContent ? node.textContent :"None", "gameId":"0"})
+    }
+    console.log(result);
+    
+    return result;
+}
+
+const getBggGamesXMLByPatten = async (pattern : string, signal: AbortSignal) => {
+    const request = axios({
+        method: "get",
+        signal: signal, //signal from controller
+        url: `${baseUrl}/search?query=${pattern}&type=boardgame`,
+    });
+
+    const response = await request;
+    return response.data;
+}
+
+
+
 const getUserByUsername = (bggUsername: string) => {
     const request = axios({
         method: "get",
@@ -53,4 +95,11 @@ const getGameImageSrcFromResponse = (res: string) : string =>{
 
 
 
-export default { getUserByUsername, getUserIdFromResponse, getBggGameById, getGameImageSrcFromResponse };
+export default {
+    getUserByUsername,
+    getUserIdFromResponse,
+    getBggGameById,
+    getGameImageSrcFromResponse,
+    getBggGamesXMLByPatten,
+    getAllGamesFromXML
+};
