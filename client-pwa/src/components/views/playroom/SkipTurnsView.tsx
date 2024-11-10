2dde521daea229d1bed4435d85cb4a31925a5f3d
ColdYellowTea <75476218+ColdYellowTea@users.noreply.github.com>
2024-11-10 17:59:54 +0100

    add validation in skip dialog

--- client-pwa/src/components/views/playroom/SkipTurnsView.tsx
+++ client-pwa/src/components/views/playroom/SkipTurnsView.tsx
@@ -13,15 +13,27 @@ interface SkipTurnsViewProps {
 
 const SkipTurnsView = ({open, setOpen, code} : SkipTurnsViewProps) => {
 
-    const [turnCount, setTurnCount] = useState(0)
+    const [turnCount, setTurnCount] = useState(1)
     const {sendJsonMessage} = useWebSocketContext();
-    const handleSkip = () =>{
-        sendJsonMessage(buildSkipOwnMoveMessage(code, turnCount))
-        setOpen(false)
+    const [error, setError] = useState<boolean>(false);
+
+    const handleSkip = () => {
+        if(error == false) {
+            sendJsonMessage(buildSkipOwnMoveMessage(code, turnCount))
+            setOpen(false)
+        }
     }
 
     const handleInputChange = (event : ChangeEvent<HTMLInputElement>) =>{
+        
+        const skipCountRegEx = /^[1-9]{1}[0-9]{0,1}$/;
         setTurnCount(parseInt(event.target.value));
+
+        if(skipCountRegEx.test(event.target.value) == true){
+            setError(false);
+        } else {
+            setError(true);
+        }
     }
 
     return(
@@ -35,15 +47,16 @@ const SkipTurnsView = ({open, setOpen, code} : SkipTurnsViewProps) => {
                 {"Skip Turns"}
                 </DialogTitle>
                 <DialogContent>
-                <DialogContentText id="alert-dialog-description">
-                    Please input number of turns and accept action.
+                <DialogContentText id="alert-dialog-description" sx={{m:0}}>
+                    Please input number of turns (1-99) and click skip.
                 </DialogContentText>
                 <Box component="form">
                 <FormControl
-                                sx={{ m: 1 }}
+                                sx={{ mt: 2 }}
                                 variant="filled"
                                 color="primary"
                                 required={true}
+                                error={error}
                             >
                                 <InputLabel  shrink htmlFor="turnsCount">
                                     Number of turns:
@@ -54,6 +67,7 @@ const SkipTurnsView = ({open, setOpen, code} : SkipTurnsViewProps) => {
                                     name="hours"
                                     value={turnCount}
                                     onChange={handleInputChange}
+                                    error={error}
                                 />
                             </FormControl>
                 </Box>
